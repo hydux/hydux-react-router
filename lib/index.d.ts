506625c9e6038709605fda666a68b3347e1cf907
zaaack <zaaack@qq.com>
2017-12-20 16:04:50 +0800

    0.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,8 @@
+import { Switch } from 'react-router';
 import { History, Location } from 'history';
-import { AppProps } from 'hydux';
-export { History, Location };
+import { App, Init, View, Subscribe, OnUpdate } from 'hydux';
+import { ActionsType } from 'hydux/lib/types';
+export { History, Location, Switch };
 export declare type RouterActions<Actions> = Actions & {
     history: {
         push(path: string, state?: any): void;
@@ -16,4 +18,13 @@ export declare type RouterState<State> = State & {
 export declare function ConnectedSwitch(props: any, {router}: {
     router: any;
 }): any;
-export default function withReactRouter<S, A>(history: History): (app: any) => (props: AppProps<S, A>) => any;
+export declare type PropsWithRouter<State, Actions> = {
+    init: Init<State, Actions>;
+    view: View<State, RouterActions<Actions>>;
+    actions: ActionsType<State, RouterActions<Actions>>;
+    subscribe?: Subscribe<State, Actions>;
+    onRender?: (view: any) => void;
+    onError?: (err: Error) => void;
+    onUpdate?: OnUpdate<State, Actions>;
+};
+export default function withReactRouter<S, A>(history: History): (app: App<RouterState<S>, RouterActions<A>>) => (props: PropsWithRouter<S, A>) => any;
